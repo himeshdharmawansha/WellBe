92f5b630b50f02e3ae64526dd1bae597703a9b07
Raveesha23 <raveeshagihani@gmail.com>
2025-01-05 12:00:39 +0530

    doctor profile handling

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -157,7 +157,7 @@ class Admin extends Controller
                $doctor = new Doctor();
 
                // Debugging: Check submitted data
-               // echo(print_r($doctorData, true));
+               //echo(print_r($doctorData, true));
 
                // Validate the input data
                if ($doctor->validateDoctor($doctorData)) {

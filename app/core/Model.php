267837693d871a386b3acfa3c3f4445b05bece23
Benshekniel Thayalan <101684571+Benshekniel@users.noreply.github.com>
2025-04-09 11:28:09 +0530

    logout function has been added

--- app/core/Model.php
+++ app/core/Model.php
@@ -128,18 +128,27 @@ class Model extends Database
 
         return false;
     }
+    
+	public function loggedin()
+	{
+		$DB = new Database();
+		// Update user state to 1 (logged in)
+		$updateStateQuery = "UPDATE user_profile SET state = 1 WHERE id = :userid";
+		$DB->write($updateStateQuery, ['userid' => $_SESSION['userid']]);
 
-    public function loggedin()
-    {
-       $DB = new Database();
-       // Update user state to 1 (logged in)
-       $updateStateQuery = "UPDATE user_profile SET state = 1 WHERE id = :userid";
-       $DB->write($updateStateQuery, ['userid' => $_SESSION['userid']]);
- 
-       // Update messages as received
-       $updateQuery = "UPDATE message SET received = 1 WHERE receiver = :receiver AND received = 0";
-       $DB->write($updateQuery, ['receiver' => $_SESSION['userid']]);
-    }
+		// Update messages as received
+		$updateQuery = "UPDATE message SET received = 1 WHERE receiver = :receiver AND received = 0";
+		$DB->write($updateQuery, ['receiver' => $_SESSION['userid']]);
+	}
+
+	public function logout()
+	{
+		$DB = new Database();
+		// Update user state to 0 (logged out)
+		$updateStateQuery = "UPDATE user_profile SET state = 0 WHERE id = :userid";
+		$DB->write($updateStateQuery, ['userid' => $_SESSION['userid']]);
+
+	}
 
     public function getAll()
     {

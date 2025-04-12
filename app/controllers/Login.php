8a9d3258b290e90209894ae0777fd5a01f285097
Benshekniel Thayalan <101684571+Benshekniel@users.noreply.github.com>
2025-04-12 12:32:31 +0530

    conflict resolved in Login.php

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -11,10 +11,7 @@ class Login extends Controller
             if (isset($_POST['nic'])) {
 
                 $id = $_POST['nic'];
-<<<<<<< HEAD
                 $_SESSION['userid'] = $id;
-=======
->>>>>>> 6a4db595ba97f516aeddf756ddbbbf8247590b55
 
                 // Check if 'd' exists in the string
                 if (strpos($id, 'd') !== false) {
@@ -24,39 +21,24 @@ class Login extends Controller
                     $user = new Patient;
                     $_SESSION['user_type'] = "patient";
                 } elseif (strpos($id, 'h') !== false) {
-<<<<<<< HEAD
                     $user = new PharmacyModel;
                     $_SESSION['user_type'] = "pharmacy";
                 } elseif (strpos($id, 'l') !== false) {
                     $user = new LabModel;
-=======
-                    $user = new Pharmacy;
-                    $_SESSION['user_type'] = "pharmacy";
-                } elseif (strpos($id, 'l') !== false) {
-                    $user = new Lab;
->>>>>>> 6a4db595ba97f516aeddf756ddbbbf8247590b55
                     $_SESSION['user_type'] = "lab";
                 } elseif (strpos($id, 'a') !== false) {
                     $user = new Admin;
                     $_SESSION['user_type'] = "admin";
                 }
 
-
                 //password_verify($_POST['password'], $row->password
                 $arr['nic'] = $_POST['nic'];
                 $row = $user->first($arr);
 
                 if ($row) {
-<<<<<<< HEAD
                     if (password_verify($_POST['password'], $row->password)) {
                         $_SESSION['USER'] = $row; // Save user details in the session
                         $model->loggedin();
-=======
-                    if (($_POST['password']== $row->password)) {
-                        $_SESSION['USER'] = $row; // Save user details in the session
-                        $model->loggedin();
-                        $_SESSION['userid'] = $row->id;
->>>>>>> 6a4db595ba97f516aeddf756ddbbbf8247590b55
                         redirect($_SESSION['user_type']);
                     } else {
                         $user->errors['password'] = 'Wrong password'; // Add specific error for wrong password

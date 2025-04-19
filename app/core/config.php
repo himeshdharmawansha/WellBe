b7f29fe99f1fadb873cc4aac047b676bcbef2cdc
Amrah444 <ammuslamath@gmail.com>
2025-04-19 17:31:03 +0530

    error1

--- app/core/config.php
+++ app/core/config.php
@@ -8,7 +8,7 @@ if ($_SERVER['SERVER_NAME'] == 'localhost') {
 	define('DBPASS', '');
 	define('DBDRIVER', '');
 
-	define('ROOT', 'http://localhost/newWellbe2/wellbe/public');
+	define('ROOT', 'http://localhost/WELLBE/public');
 } else {
 	/** database config **/
 	define('DBNAME', 'wellbe_db');

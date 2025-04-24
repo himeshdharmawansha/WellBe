b5bcec544f56ec694f77cb6bdb080722af8415a4
Benshekniel Thayalan <101684571+Benshekniel@users.noreply.github.com>
2025-04-24 10:10:59 +0530

    forgot password 3

--- app/views/forgot.view.php
+++ app/views/forgot.view.php
@@ -22,15 +22,15 @@
       <?php if (!empty($errorMessage)): ?>
         <p class="forgot-message"><?= htmlspecialchars($errorMessage) ?></p>
       <?php endif; ?>
-
-      <input type="submit" name="check" value="Check">
-
       <div class="signup_link">
         Back to login? <a href="<?= ROOT ?>/login"> Login </a>
       </div>
       <div class="forgotpass">
         <p>Don't have an Account?<br> Create your Profile <span><a href="<?= ROOT ?>/signup">Click here</a></span></p>
       </div>
+
+      <input type="submit" name="check" value="Check">
+
     </form>
 
   </div>
